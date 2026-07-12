95ecb8a9b8d99c76a0d496b118c76777c65a9e38
Jaya sharma <imsharmajaya@gmail.com>
2026-07-12 23:38:12 +0530

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,19 @@
+
+public class Comments {
+    public static void main(String[] args){
+        //this is important,dont delete this
+        System.out.println("hello world");
+
+         /*
+         this is the first line of the comment
+         the comment is still continuing
+         
+         */
+        int a = 5;
+        
+    }
+
+
+
+    
+}
