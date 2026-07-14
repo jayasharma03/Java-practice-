7790be05c6f17e37701acc7f742f9ae9ba54bb00
Jaya sharma <imsharmajaya@gmail.com>
2026-07-14 22:49:37 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,23 @@
+
+public class variables {
+    public static void main(String[] args) {
+    int mynumber = 865;
+     int yournumber;
+     yournumber = 865;
+    System.out.println(yournumber);
+    yournumber = 45;
+    System.out.println(yournumber);
+
+    float myfloat = 5;
+    double myDouble =6.;
+    System.out.println(myDouble);
+    System.out.println(myfloat);
+    boolean isvegetarian = false;
+    System.out.println(isvegetarian);
+    String wishes = "Good Morning";
+    System.out.println(wishes);
+    char mychar = 'k';
+    System.out.println(mychar);
+
+}
+}
