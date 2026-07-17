403f534b91ca820d1388347b23bb4e461af70fe9
Jaya sharma <imsharmajaya@gmail.com>
2026-07-17 20:28:36 +0530

    Add files via upload

---
+++ righthavepyramid.java
@@ -0,0 +1,9 @@
+public class righthavepyramid{
+    public static void main(String[]args){
+        System.out.println("* * * * *");
+        System.out.println("* * * * ");
+         System.out.println("* * * ");
+          System.out.println("* * ");
+          System.out.println("*");
+    }
+}
