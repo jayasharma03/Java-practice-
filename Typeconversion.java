8d689336e010cdf3278b544fd6155409fe80b5bf
Jaya sharma <imsharmajaya@gmail.com>
2026-07-18 23:04:08 +0530

    Add files via upload

---
+++ Typeconversion.java
@@ -0,0 +1,13 @@
+
+
+public class Typeconversion {
+    public static void main(String[] args){
+        float myfloat = 9;
+        System.out.println(myfloat);
+        int myInt = (int) 5.45f;
+        System.out.println(myInt);
+
+
+}
+
+}
